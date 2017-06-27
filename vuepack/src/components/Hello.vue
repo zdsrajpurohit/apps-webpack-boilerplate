1252527b252cb4c8145dc315f7ba7510543b21a1
Shakti <shaktirp1@gmail.com>
2017-06-27 15:15:54 -0700

    added dsstore

---
+++ vuepack/src/components/Hello.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      userName: ''
+    }
+  },
+  computed: {
+    msg: function () {
+      return 'Hello ' + this.userName + ', welcome to Zendesk Vue.js App.'
+    }
+  },
+  methods: {
+    getUser: function () {
+      client.get('currentUser').then(
+        data => { this.userName = data['currentUser']['name'] }
+      )
+    }
+  },
+  mounted: function () {
+    this.getUser()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+p {
+  color: #bd322c;
+}
+</style>
